b2b2a201e23c9838a2533a8757d4872dcf8be444
José Brito <josebritof.09@gmail.com>
2025-08-30 20:04:38 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "lib/curl/curl.h"
+#include "lib/json.hpp"
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+using json = nlohmann::json;
+
+// API key: 6a23773c0153f0beac3cf62e6df26495
+// Compile: g++ weatherTest_lcurl.cpp -o weatherTest_lcurl.exe -lcurl
+
+int main()
+{
+    
+    return 0;
+}
